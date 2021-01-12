cd7fd3422824b04ff8bffe6b81a1c2bd6cb032fd
atk3000 <kou.music0318@gamil.com>
2021-01-12 12:13:25 +0900

    追加テーブルの設計ver2

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -17,8 +17,10 @@ CREATE TABLE `ec_details` (
     `order_id` int(11) NOT NULL,
     `item_id` int(11) NOT NULL,
     `amount` int(11) NOT NULL,
-    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `price` int(11) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `ec_details`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  ADD PRIMARY KEY (`order_id`),
   ADD KEY `item_id` (`item_id`);
